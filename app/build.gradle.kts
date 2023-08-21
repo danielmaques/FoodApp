dcf848afe53348f3dd85c4b34dc990c28cc0acac
Daniel Marques <76397634+danielmaques@users.noreply.github.com>
2023-08-21 13:09:00 -0300

    nav bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.foodsapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.foodsapp"
@@ -42,59 +42,14 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     /// Coordinator Layout
-    implementation ("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
+    implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
 
     /// Material Components
-    implementation ("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.9.0")
 
-    // Navigation Component
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.3.0")
-    implementation ("androidx.navigation:navigation-ui-ktx:2.3.0")
-
-    // Room components
-    implementation ("androidx.room:room-runtime:2.2.5")
-    implementation ("androidx.room:room-ktx:2.2.5")
-    androidTestImplementation ("androidx.room:room-testing:2.2.5")
-
-    // DataStore
-    implementation ("androidx.datastore:datastore-preferences:1.0.0-alpha01")
-
-    // Recyclerview
-    implementation ("androidx.recyclerview:recyclerview:1.1.0")
-
-    // Retrofit
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-
-    //Dagger - Hilt
-    implementation ("com.google.dagger:hilt-android:2.28-alpha")
-
-    implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha02")
-
-    // Coroutines
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
-
-    // Lifecycle
-    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.2.0")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.2.0")
-
-    // Image Loading library Coil
-    implementation ("io.coil-kt:coil:0.13.0")
-
-    // Gson
-    implementation ("com.google.code.gson:gson:2.8.6")
-
-    // Shimmer
-    implementation ("com.facebook.shimmer:shimmer:0.5.0")
-    implementation ("com.todkars:shimmer-recyclerview:0.4.0")
-
-    // Jsoup
-    implementation ("org.jsoup:jsoup:1.13.1")
-
-
+    /// Navigation component
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.0")
 
 
     testImplementation("junit:junit:4.13.2")
