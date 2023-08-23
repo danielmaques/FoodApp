e7a6b1070d12ab19030bb32e08edb2f685a404c8
Daniel Marques <76397634+danielmaques@users.noreply.github.com>
2023-08-22 22:47:34 -0300

    shimmer

--- app/src/main/java/com/example/foodsapp/RecipesFragment.kt
+++ app/src/main/java/com/example/foodsapp/RecipesFragment.kt
@@ -5,14 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.facebook.shimmer.ShimmerFrameLayout
 
 class RecipesFragment : Fragment() {
+
+    private lateinit var shimmerFrameLayout: ShimmerFrameLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
+
+        shimmerFrameLayout = view.findViewById(R.id.shimmerFrameLayout)
+
+        return view
+    }
+
+    override fun onResume() {
+        super.onResume()
+        shimmerFrameLayout.startShimmer()
+    }
+
+    override fun onPause() {
+        shimmerFrameLayout.stopShimmer()
+        super.onPause()
     }
 }
