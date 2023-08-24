5c70465eec90ed05e7ee7df4fa512e7745f8ed21
Daniel Marques <76397634+danielmaques@users.noreply.github.com>
2023-08-24 17:13:38 -0300

    Network

---
+++ app/src/main/java/com/example/foodsapp/MainViewModel.kt
@@ -0,0 +1,77 @@
+package com.example.foodsapp
+
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.foodsapp.data.Repository
+import com.example.foodsapp.models.FoodRecipes
+import com.example.foodsapp.uitl.NetworkResult
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class MainViewModel @ViewModelInject constructor(
+    private val repository: Repository,
+    application: Application
+) : AndroidViewModel(application) {
+
+    var recipesResponse: MutableLiveData<NetworkResult<FoodRecipes>> = MutableLiveData()
+
+    fun getRecipes(queries: Map<String, String>) = viewModelScope.launch {
+        getRecipesSafeCall(queries)
+    }
+
+    private suspend fun getRecipesSafeCall(queries: Map<String, String>) {
+        recipesResponse.value = NetworkResult.Loading()
+        if (hasInternetConnection()) {
+            try {
+                val response = repository.remote.getRecipes(queries)
+                recipesResponse.value = handleFoodRecipes(response)
+            } catch (e: Exception) {
+                recipesResponse.value = NetworkResult.Error("Something went wrong")
+            }
+        } else {
+            recipesResponse.value = NetworkResult.Error("No internet connection")
+        }
+    }
+
+    private fun handleFoodRecipes(response: Response<FoodRecipes>): NetworkResult<FoodRecipes> {
+        when {
+
+            response.message().toString().contains("timeout") -> {
+                return NetworkResult.Error("Timeout")
+            }
+
+            response.code() == 402 -> {
+                return NetworkResult.Error("API Key not found")
+            }
+
+            response.isSuccessful -> {
+                return NetworkResult.Success(response.body()!!)
+            }
+
+            else -> {
+                return NetworkResult.Error(response.message())
+            }
+        }
+    }
+
+    private fun hasInternetConnection(): Boolean {
+        val connection = getApplication<Application>().getSystemService(
+            Context.CONNECTIVITY_SERVICE
+        ) as ConnectivityManager
+        val activeNetwork = connection.activeNetwork ?: return false
+        val capabilities = connection.getNetworkCapabilities(activeNetwork) ?: return false
+
+        return when {
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
+    }
+}
